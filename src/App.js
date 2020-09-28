134a2d19a8e7765781ed55ce490e8a390708114f
Zsolt Zsombor <zsolt.zsombor@abrisconsult.com>
2020-09-28 08:30:33 +0200

    user name

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { Auth } from '@aws-amplify/auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { user: '' };
+  }
+
+  async componentDidMount() {
+    const userInfo = await Auth.currentUserInfo();
+    this.setState({ user: userInfo.username });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!</h1>
+          <p>{this.state.user}</p>
+        </header>
+        <AmplifySignOut />
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App);
