3b7050ba055f845c382d05af1cc4c630aba4eff6
Zsolt Zsombor <zsolt.zsombor@abrisconsult.com>
2020-09-30 07:25:17 +0200

    https and App func

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { Auth } from "@aws-amplify/auth";
 import axios from "axios";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  const [userName, setUserName] = useState("");
 
-    this.state = { user: "" };
-  }
+  useEffect(() => {
+    async function getUserName() {
+      const userInfo = await Auth.currentUserInfo();
+      const answer = await axios.get(
+        `https://awscdci3-env.eba-hf4peyrq.eu-central-1.elasticbeanstalk.com/hello?name=${userInfo.username}`
+      );
+      setUserName(answer.data);
+    }
+    getUserName();
+  });
 
-  async componentDidMount() {
-    const userInfo = await Auth.currentUserInfo();
-    const answer = await axios.get(
-      `http://awscdci3-env.eba-hf4peyrq.eu-central-1.elasticbeanstalk.com/hello?name=${userInfo.username}`
-    );
-    console.log(answer);
-
-    this.setState({ user: answer.data });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>We now have Auth!</h1>
-          <p>{this.state.user}</p>
-        </header>
-        <AmplifySignOut />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+        <p>{userName}</p>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
 }
 
 export default withAuthenticator(App);
